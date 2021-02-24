ec75996dae96701df9541afec7bf6b98c2b7113b
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-24 18:56:47 -0300

    Aulas 15 - JavaScript das bolinhas seletoras de cor

--- javascript/block/card.js
+++ javascript/block/card.js
@@ -2,6 +2,9 @@
 
 var $bolinha = document.querySelectorAll('.bolinha'); // guarda na var $bolinha todos os elementos que possuem a classe bolinha
 //console.log($bolinha[0]);
-$bolinha[0].addEventListener('click', function(){ // coloquei uma escuta na primeira bolinha do primeiro card
-    console.log('Você clicou no primeiro elemento');
-});
+
+for (let index = 0; index < $bolinha.length; index++) { // percorre escuta em todas as bolinhas de cor
+    $bolinha[index].addEventListener('click', function(){ // coloquei uma escuta na primeira bolinha do primeiro card
+        console.log('Você clicou !!');
+    });    
+}
