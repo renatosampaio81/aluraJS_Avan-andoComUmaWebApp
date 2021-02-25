01bc96f1088d79c69005ddd0b88882082c417a57
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-25 07:35:27 -0300

    Aulas 17 - JavaScript das bolinhas seletoras de cor e dos cards

--- javascript/block/card.js
+++ javascript/block/card.js
@@ -5,6 +5,10 @@ var $bolinha = document.querySelectorAll('.bolinha'); // guarda na var $bolinha 
 
 for (let index = 0; index < $bolinha.length; index++) { // percorre escuta em todas as bolinhas de cor
     $bolinha[index].addEventListener('click', function(){ // coloquei uma escuta na primeira bolinha do primeiro card
-        console.log('Você clicou !!');
+        //console.log('Você clicou em uma bolinha!!');
+        var $card = this.parentNode.parentNode.parentNode; //this é a bolinha (ou li), parentNode sobe para o pai, logo, é a ul. Como estou subindo 3x estou chegando no card.
+        //console.log($card.dataset.cardcolor); //dataset retorna um json daquela propriedade data- que eu criei dentro de cada card, quando eu especifico que eu quero o cardcolor ele vai trazer o conteúdo
+        $card.dataset.cardcolor = this.dataset.cardcolor; //aqui ele vai atribuir à propriedade cardcolor do card o valor do cardcolor da bolinha clicada
+        //console.log($card.dataset.cardcolor);
     });    
 }
