14c94b04cef59c21337df2565495387ae984133d
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-25 22:02:58 -0300

    Aulas 19 - JavaScript das bolinhas seletoras de cor e dos cards

--- javascript/block/card.js
+++ javascript/block/card.js
@@ -1,20 +1,22 @@
 /* JavaScript do Bloco de Cartões */
 
-var $bolinha = document.querySelectorAll('.bolinha'); // guarda na var $bolinha todos os elementos que possuem a classe bolinha
-//console.log($bolinha[0]);
+var $cards = document.querySelectorAll('.card'); // guarda na var $card todos os elementos que possuem a classe card
+var $bolinhas = document.querySelectorAll('.bolinha'); // guarda na var $bolinha todos os elementos que possuem a classe bolinha
 
-for (let index = 0; index < $bolinha.length; index++) { // percorre todas as bolinhas de cor
-    $bolinha[index].addEventListener('click', function(){ // coloquei uma escuta em todas as bolinhas, quando clicarem vai rodar a function
-        //console.log('Você clicou em uma bolinha!!');
-        var $card = this.parentNode.parentNode.parentNode; //this é a bolinha (ou li), parentNode sobe para o pai, logo, é a ul. Como estou subindo 3x estou chegando no card.
-        //console.log($card.dataset.cardcolor); //dataset retorna um json daquela propriedade data- que eu criei dentro de cada card, quando eu especifico que eu quero o cardcolor ele vai trazer o conteúdo
-        $card.dataset.cardcolor = this.dataset.cardcolor; //aqui ele vai atribuir à propriedade cardcolor do card clicado o valor do cardcolor da bolinha clicada
-        //console.log($card.dataset.cardcolor);
-        for (var i = 0; i < $bolinha.length; i++) { //esse for vai ser rodado no click da bolinha e vai correr todas as bolinhas removendo o isActive
-            $bolinha[i].classList.remove('isActive');
+for (let index = 0; index < $cards.length; index++) { // percorre todos os cards
+    $cards[index].addEventListener('click', function(){ // coloquei uma escuta em todos cards, quando clicarem vai rodar a function
+        var $this = event.target; // o event.target traz o elemento que foi clicado dentro do target, no caso queremos as bolinhas de seleção de cor
+        
+        if ($this.classList.contains('bolinha')) { // verifica se o objeto clicado tem a classe bolinha. Se não for ele vai ignorar a rotina abaixo
+            var $card = this // this é o card
+
+            $card.dataset.cardcolor = $this.dataset.cardcolor; // aqui ele vai atribuir à propriedade cardcolor do card clicado o valor do cardcolor da bolinha clicada
+        
+            for (var i = 0; i < $bolinhas.length; i++) { //esse for vai ser rodado no click da bolinha e vai correr todas as bolinhas removendo o isActive
+                $bolinhas[i].classList.remove('isActive');
+            }
+
+            $this.classList.add('isActive'); //adiciona a classe isActive à bolinha que foi clicada    
         }
-        this.classList.add('isActive'); //adiciona a classe isActive à bolinha que foi clicada
-
-
-    });    
+    });
 }
