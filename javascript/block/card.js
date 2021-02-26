09a3f999f2bf895db1f8e2abfc478c1b5c168870
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-26 08:06:34 -0300

    Aulas 21 - Implementada rotina para excluir card

--- javascript/block/card.js
+++ javascript/block/card.js
@@ -6,9 +6,9 @@ var $bolinhas = document.querySelectorAll('.bolinha'); // guarda na var $bolinha
 for (let index = 0; index < $cards.length; index++) { // percorre todos os cards
     $cards[index].addEventListener('click', function(){ // coloquei uma escuta em todos cards, quando clicarem vai rodar a function
         var $this = event.target; // o event.target traz o elemento que foi clicado dentro do target, no caso queremos as bolinhas de seleção de cor
-        
-        if ($this.classList.contains('bolinha')) { // verifica se o objeto clicado tem a classe bolinha. Se não for ele vai ignorar a rotina abaixo
-            var $card = this // this é o card
+        var $card = this // this é o card
+
+        if ($this.dataset.cardcolor) { // verifica se o objeto clicado tem a a propriedade dataColor. Se não for ele vai ignorar a rotina abaixo
 
             $card.dataset.cardcolor = $this.dataset.cardcolor; // aqui ele vai atribuir à propriedade cardcolor do card clicado o valor do cardcolor da bolinha clicada
         
@@ -17,6 +17,10 @@ for (let index = 0; index < $cards.length; index++) { // percorre todos os cards
             }
 
             $this.classList.add('isActive'); //adiciona a classe isActive à bolinha que foi clicada    
+        }
+
+        if ($this.classList.contains('card_delete')) { //verifica se o objeto clicado tem a a classe card_delete. Se não for ele vai ignorar a rotina abaixo
+            $card.remove();
         }
     });
 }
