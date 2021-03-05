00204d4326d880647a98a0e14d76f7bb01f2e9e0
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-03-05 13:50:37 -0300

    Aulas 29 - Trabalhando com a validação do submit para criação do novo cartao

--- javascript/block/newCard.js
+++ javascript/block/newCard.js
@@ -3,11 +3,23 @@
 /* o $ significa que a var guarda um elemento do HTML */
 var $newCard = document.querySelector('.newCard');
 var $newCardContent = document.querySelector('.newCard-content');
+var $newCardAction = document.querySelector('.newCard-action');
 
 $newCard.addEventListener('submit',function(event) { // fica ouvindo o newcard, ao executar o submit ele executa a função
     if ($newCardContent.value == '') { //se o form de criação de card estiver vazio
         event.preventDefault(); //evita o comportamento padrao de enviar o formulário ao clicar em submit
-        console.log('está vazio !');
+        //console.log('está vazio !');
+        
+        if (document.querySelector('.error') == null) { //verifica se ja existe um objeto com a classe error criado
+            var $error = document.createElement('span'); //cria um elemento spam caso o form esteja vazio
+            $error.classList.add('error'); // crio uma classe error para o span
+            $error.textContent = 'Por favor, preencha o campo acima'; //o conteúdo do spam
+            //pra colocar um conteudo dentro de um formulario (input, ou tudo dentro de um form).. se utiliza value
+            //pra colocar conteúdo dentro de parágrafo, spam, h1.. se utiliza o textContent
+            $newCard.insertBefore($error, $newCardAction);
+            // insere lá no html o span que foi criado ANTES do botao submit (que lá é um input)
+            // IMPORTANTE, o insertBefore tem que ser uma propriedade do pai do input, que no caso é o form (newcard) 
+        }
     }
 })
 
